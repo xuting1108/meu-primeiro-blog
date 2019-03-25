fafd44b6891b435df98acc44133baaa38158c372
xuting1108 <xuting1108@hotmail.com>
2019-03-25 09:45:08 -0300

    modificação de templates para mostrar os posts da base de dados

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+#O ponto antes de models significa diretório atual ou aplicativo atual. Tanto views.py como models.py estão no mesmo diretório. Isto significa que podemos usar . e o nome do arquivo (sem py).
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
 #função (def) chamada post_list que recebe um request, executa a função render que irá renderizar (montar) nosso modelo de acordo com o template blog/post_list.html, e retorna (return) o resultado.
